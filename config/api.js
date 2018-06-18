b962a552989ea206b25d3d5b8834720265ba4967
Ravivarman Perinpanathan <ravivarman.perinpanathan@epitech.eu>
2018-06-18 04:19:08 +0200

    news view

--- config/api.js
+++ config/api.js
@@ -6,12 +6,17 @@ var api = {
         return fetch(url).then((res)=> res.json());
     
     },
-    /*getImages(){
+    getImages(){
          //var url = 'https://api.nasa.gov/mars-photos/api/v1/rovers?api_key=6LS6npEUUHvzkQ3gbV94pwBmicR4KDb92Qx3fCik';
          //var url = 'https://api.nasa.gov/planetary/apod?api_key=6LS6npEUUHvzkQ3gbV94pwBmicR4KDb92Qx3fCik';
-         var url = 'https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&api_key=6LS6npEUUHvzkQ3gbV94pwBmicR4KDb92Qx3fCik';
+         //var url = 'https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&api_key=6LS6npEUUHvzkQ3gbV94pwBmicR4KDb92Qx3fCik';
+         var url = 'https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&page=2&api_key=6LS6npEUUHvzkQ3gbV94pwBmicR4KDb92Qx3fCik'
          return fetch(url).then((res)=> res.json());
-    }*/
+    },
+    getBbc(){
+        var url = 'https://newsapi.org/v2/top-headlines?sources=bbc-news&apiKey=44d043ee9dcd436da97fc71880cf3a1b';
+        return fetch(url).then((res)=> res.json());
+    }
 };
 
 module.exports = api;
--- src/Home/Home.js
+++ src/Home/Home.js
@@ -1,134 +1,81 @@
 import React, { Component } from 'react';
-
-import { Text, View, StyleSheet, Image, FlatList  } from 'react-native';
-
+import { Text, View, StyleSheet, Image, FlatList, WebView } from 'react-native';
 import api from '../../config/api';
-
 import { Card, ListItem, Button, Icon } from 'react-native-elements'
-
-const users = [
-    {
-       name: 'brynn',
-       avatar: 'https://s3.amazonaws.com/uifaces/faces/twitter/brynn/128.jpg'
-    }
-   ]
-   
-
-
 
 class Home extends Component {
 
-  constructor(props){
-    super(props);
-    this.state ={
-      id: []
+  constructor() {
+    super();
+    this.state = {
+      data: []
     }
   }
 
-  componentWillMount(){
-    api.getRovers().then((res) =>{
-      this.setState({
-        copyright:res.copyright,
-        hdurl:res.hdurl,
-        date:res.date,
-        explanation:res.explanation,
-        title:res.title
-      })
-    });
-    /*api.getImages().then((res) =>{
-      this.setState({
-        data:res.photos
-      })
-    });*/
+  renderItem = ({ item }) => {
+    return (
+      <View style={{ flex: 1, flexDirection: 'row' }}>
+        <Card
+          title={item.title}
+          image={{ uri:item.urlToImage }}
+          containerStyle={{ padding: 0, marginTop: 50 }}>
+          <Text style={{ marginBottom: 10 }}>
+            {item.description}
+          </Text>
+          <WebView
+            source={{ uri: item.url }}
+            style={{ marginTop: 20 }}
+          />
+          <Button
+            icon={<Icon name='code' color='#ffffff' />}
+            backgroundColor='#03A9F4'
+            buttonStyle={{ borderRadius: 5, marginLeft: 0, marginRight: 0, marginBottom: 0 }}
+            title='VIEW NOW' />
+        </Card>
+      </View>
+    )
   }
 
-    render() {
-        console.log("Rovers: ", this.state.id);
-        /*return (
-          <View>
-            <FlatList>
-              data={this.state.data}
-              renderItem={({photos}) =>
-                <View>
-                  <Text style={{marginBottom: 10}}>
-                    {photos.id}
-                  </Text>
-                </View>
-            }
-            </FlatList>
-          </View>
-          
-        )*/
-        return (
-          <View>
-          <Card
-            title={this.state.title}
-            image={this.state.hdurl}
-            containerStyle={{padding: 0, marginTop: 50}}>
-            <Text style={{marginBottom: 10}}>
-            {this.state.explanation}
-            </Text>
-            <Button
-              icon={<Icon name='code' color='#ffffff' />}
-              backgroundColor='#03A9F4'
-              buttonStyle={{borderRadius: 5, marginLeft: 0, marginRight: 0, marginBottom: 0}}
-              title='VIEW NOW' />
-          </Card>
-           <Card containerStyle={{padding: 0, marginTop: 10}} >
-           {
-             users.map((u, i) => {
-               return (
-                 <ListItem
-                   key={i}
-                   roundAvatar
-                   title={this.state.title}
-                   avatar={this.state.hdurl}
-                 />
-               );
-             })
-           }
-         </Card>
-         </View>
-        )
-      }
+  componentWillMount() {
+    api.getBbc().then((res) => {
+      this.setState({
+        data: res.articles
+      })
+    });
+  }
+
+  render() {
+    //console.log("Rovers: ", this.state.data);
+    return (
+      <View>
+        <FlatList
+          data={this.state.data}
+          renderItem={this.renderItem}
+          keyExtractor={() => Math.random().toString(36).substr(2, 9)}
+        />
+      </View>
+    )
+  }
 }
 
 const styles = StyleSheet.create({
-    card: {
-      flex: 1
-    },
-   user: {
-      padding: 20
-     },
-     image:{
-         height: 100,
-         backgroundColor: 'rgba(225,225,225,0.2)',
-         marginBottom: 10,
-         padding: 10,
-         color: '#fff'
-     },
-     name:{
-         backgroundColor: '#2980b6',
-         paddingVertical: 15
-     }
+  card: {
+    flex: 1
+  },
+  user: {
+    padding: 20
+  },
+  image: {
+    height: 100,
+    backgroundColor: 'rgba(225,225,225,0.2)',
+    marginBottom: 10,
+    padding: 10,
+    color: '#fff'
+  },
+  name: {
+    backgroundColor: '#2980b6',
+    paddingVertical: 15
+  }
 });
 
-   
-   // implemented without image with header
-   /*
-    <Card
-            title={this.state.title}
-            image={this.state.hdurl}
-            containerStyle={{padding: 0, marginTop: 50}}>
-            <Text style={{marginBottom: 10}}>
-            {this.state.explanation}
-            </Text>
-            <Button
-              icon={<Icon name='code' color='#ffffff' />}
-              backgroundColor='#03A9F4'
-              buttonStyle={{borderRadius: 5, marginLeft: 0, marginRight: 0, marginBottom: 0}}
-              title='VIEW NOW' />
-          </Card>
-   */
-
-   export default Home;
+export default Home;
