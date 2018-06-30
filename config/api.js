bf0e0e1a41fbebc5adc06a46ab9d104d4116eaff
Ravivarman Perinpanathan <ravivarman.perinpanathan@epitech.eu>
2018-06-30 13:55:56 +0200

    bugs corrected

--- config/api.js
+++ config/api.js
@@ -1,15 +1,10 @@
 var api = {
     getRovers(){
-        //var url = 'https://api.nasa.gov/mars-photos/api/v1/rovers?api_key=6LS6npEUUHvzkQ3gbV94pwBmicR4KDb92Qx3fCik';
         var url = 'https://api.nasa.gov/planetary/apod?api_key=6LS6npEUUHvzkQ3gbV94pwBmicR4KDb92Qx3fCik';
-        //var url = 'https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&api_key=6LS6npEUUHvzkQ3gbV94pwBmicR4KDb92Qx3fCik';
         return fetch(url).then((res)=> res.json());
     
     },
     getImages(){
-         //var url = 'https://api.nasa.gov/mars-photos/api/v1/rovers?api_key=6LS6npEUUHvzkQ3gbV94pwBmicR4KDb92Qx3fCik';
-         //var url = 'https://api.nasa.gov/planetary/apod?api_key=6LS6npEUUHvzkQ3gbV94pwBmicR4KDb92Qx3fCik';
-         //var url = 'https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&api_key=6LS6npEUUHvzkQ3gbV94pwBmicR4KDb92Qx3fCik';
          var url = 'https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&page=2&api_key=6LS6npEUUHvzkQ3gbV94pwBmicR4KDb92Qx3fCik'
          return fetch(url).then((res)=> res.json());
     },
@@ -22,5 +17,4 @@ var api = {
         return fetch(url).then((res)=> res.json());
     }
 };
-
 module.exports = api;
--- src/Views/WallStreet.js
+++ src/Views/WallStreet.js
@@ -1,49 +1,56 @@
 import React, { Component } from 'react';
-import { Text, View, StyleSheet, Image, FlatList, WebView, Linking} from 'react-native';
+import {
+  Text, View, StyleSheet, Image, FlatList, WebView, Linking,
+} from 'react-native';
+import {
+  Card, ListItem, Button, Icon, SCREEN_WIDTH, SCREEN_HEIGHT,
+} from 'react-native-elements';
 import api from '../../config/api';
-import { Card, ListItem, Button, Icon, SCREEN_WIDTH, SCREEN_HEIGHT } from 'react-native-elements'
 
 class WallStreet extends Component {
-
   constructor() {
     super();
     this.state = {
-      data: []
-    }
+      data: [],
+    };
   }
 
-  renderItem = ({ item }) => {
-    return (
-      <View style={{ flex: 1, flexDirection: 'row'}}>
-        <Card style={{width:50, height:50}}
-          title={item.title}
-          image={{ uri:item.urlToImage }}
-          imageStyle={{width:345, height:200, alignItems: 'center',}}
-          containerStyle={{ padding: 0, marginTop: 50 }}>
-          <Text style={{ marginBottom: 10 }}>
-            {item.description}
-          </Text>
-          
-          <Button onPress={()=>{ Linking.openURL(item.url)}}
-            icon={<Icon name='code' color='#ffffff' />}
-            backgroundColor='#03A9F4'
-            buttonStyle={{ borderRadius: 5, marginLeft: 0, marginRight: 0, marginBottom: 0 }}
-            title='READ MORE' />
-        </Card>
-      </View>
-    )
-  }
+  renderItem = ({ item }) => (
+    <View style={{ flex: 1, flexDirection: 'row' }}>
+      <Card
+        style={{ width: 50, height: 50 }}
+        title={item.title}
+        image={{ uri: item.urlToImage }}
+        imageStyle={{ width: 345, height: 200, alignItems: 'center' }}
+        containerStyle={{ padding: 0, marginTop: 50 }}
+      >
+        <Text style={{ marginBottom: 10 }}>
+          {item.description}
+        </Text>
+
+        <Button
+          onPress={() => { Linking.openURL(item.url); }}
+          icon={<Icon name="code" color="#ffffff" />}
+          backgroundColor="#03A9F4"
+          buttonStyle={{
+            borderRadius: 5, marginLeft: 0, marginRight: 0, marginBottom: 0,
+          }}
+          title="READ MORE"
+        />
+      </Card>
+    </View>
+  )
 
   componentWillMount() {
     api.getWallStreet().then((res) => {
       this.setState({
-        data: res.articles
-      })
+        data: res.articles,
+      });
     });
   }
 
   render() {
-    //console.log("Rovers: ", this.state.data);
+    // console.log("Rovers: ", this.state.data);
     return (
       <View>
         <FlatList
@@ -52,7 +59,7 @@ class WallStreet extends Component {
           keyExtractor={() => Math.random().toString(36).substr(2, 9)}
         />
       </View>
-    )
+    );
   }
 }
 
@@ -63,20 +70,20 @@ const styles = StyleSheet.create({
     height: 100,
   },
   user: {
-    padding: 20
+    padding: 20,
   },
   image: {
     height: 100,
-    width:50,
+    width: 50,
     backgroundColor: 'rgba(225,225,225,0.2)',
     marginBottom: 10,
     padding: 10,
-    color: '#fff'
+    color: '#fff',
   },
   name: {
     backgroundColor: '#2980b6',
-    paddingVertical: 15
-  }
+    paddingVertical: 15,
+  },
 });
 
 export default WallStreet;
